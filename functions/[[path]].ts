ba16bc53e1cdf77e1b399db397f28cd3a2e17376
ASgith <its.angola@gmail.com>
2025-08-27 16:55:55 +0100

    Iniciando deploy appdev

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -2,11 +2,22 @@ import type { ServerBuild } from '@remix-run/cloudflare';
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 
 export const onRequest: PagesFunction = async (context) => {
-  const serverBuild = (await import('../build/server')) as unknown as ServerBuild;
+  try {
+    const serverBuild = (await import('../build/server')) as unknown as ServerBuild;
 
-  const handler = createPagesFunctionHandler({
-    build: serverBuild,
-  });
+    const handler = createPagesFunctionHandler({
+      build: serverBuild,
+    });
 
-  return handler(context);
+    return handler(context);
+  } catch (error) {
+    console.error('Function handler error:', error);
+    
+    return new Response('Internal Server Error', {
+      status: 500,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    });
+  }
 };
